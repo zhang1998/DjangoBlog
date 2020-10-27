0df759cad10eca6aae941040367ddc978f39bfde
zhang1998 <2764207312@qq.com>
2020-10-27 20:53:04 +0800

    DONE 准备内容

--- comic/urls.py
+++ comic/urls.py
@@ -19,6 +19,7 @@ from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
+    re_path(r'',TemplateView.as_view(template_name="comic/addNewGroup.html"),name='Others_done'),   # 显示最基础的模板
     re_path(r'',TemplateView.as_view(template_name="comic/test.html"),name='Others_done'),   # 显示最基础的模板
 
 ]
